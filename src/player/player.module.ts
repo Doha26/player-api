166452c51098c32776f7b958df4a5e69c055db35
Pavel FOUJEU <foujeupavel@gmail.com>
2023-02-09 10:09:52 +0100

    Update player service

--- src/player/player.module.ts
+++ src/player/player.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PlayerService } from './player.service';
 import { PlayerController } from './player.controller';
+import { S3Provider } from './s3.provider';
 
 @Module({
   controllers: [PlayerController],
-  providers: [PlayerService],
+  providers: [...S3Provider, PlayerService],
+  exports: [...S3Provider],
 })
 export class PlayerModule {}
